8702e0d3748734bca99c84da75806db1554600fb
SalmanHossen <hossainsalman873@gmail.com>
2026-01-15 01:20:54 +0600

    fix in app.js file

--- server/app.js
+++ server/app.js
@@ -19,8 +19,7 @@ const app=express();
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
-    "https://event-listing-platform-psi.vercel.app"
+    "http://localhost:5173"
   ],
   credentials: true
 }));
